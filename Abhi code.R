e673075ecafe30212b8de5b0fdbe464ef22d1e69
Abhishek ANgadi <aba32@duke.edu>
2018-11-08 01:42:48 -0500

    change test 1

--- Abhi code.R
+++ Abhi code.R
@@ -1,1 +1,117 @@
-Github test
+#My code
+#install.packages("sentimentr")
+
+library(zipcode)
+library(tidyverse)
+library(usmap)
+library(ggplot2)
+library(tidytext)
+library(tm)
+library(topicmodels)
+library(wordcloud)
+library(RColorBrewer)
+library(readxl)
+library(stringr)
+library(tidyr)
+
+
+sample2 <- read_excel(("Real Talk Data (1).xlsx"))
+
+pub_data <- sample2[sample2$Subject == "puberty",]
+pub_data <- pub_data[is.na(pub_data[,33]),]
+
+pub_data$Story <- do.call(paste, c(pub_data[37:45], sep=" "))
+pub_data$Story <- gsub("NA","",pub_data$Story)
+colnames(pub_data)[2] <- c("Submission")
+
+
+pub_data$Story <- trimws(pub_data$Story)
+
+pub_data <- pub_data[pub_data$Story != "",]
+
+#########
+#grouping methods - sentiment analysis
+
+undesirable_words <- c("the")
+
+
+pub_tidy <- pub_data %>%
+  unnest_tokens(word, Story) %>% #Break the lyrics into individual words
+  filter(!word %in% undesirable_words) %>% #Remove undesirables
+  filter(!nchar(word) < 2) %>% #Words like "on" or "bc" used in lingo
+  anti_join(stop_words) #Data provided by the tidytext package
+
+pub_bing <- pub_tidy %>%
+  inner_join(get_sentiments("bing"))
+
+#str(pub_bing)
+
+ind_polarity_bing <- pub_bing %>%
+  group_by(Submission, sentiment) %>%
+  count(Submission, sentiment) %>%
+  spread(sentiment, n) %>%
+  mutate(polarity = positive - negative,
+         ratio = polarity / (positive + negative))
+
+
+########
+# sentiment sentence wise
+
+library(sentimentr)
+text <- data.frame(id = c("12345"),
+                   sentence = c("Talk shit and tried to fight"),
+                   stringsAsFactors = FALSE)
+
+checkscore <- sentiment(text$sentence)
+score <- checkscore$sentiment/checkscore$word_count*100
+
+sentiment_polarity_score <- sentiment(pub_data$Story)
+#sentiment_polarity_score2 <- mutate(sentiment = get_sentiment(pub_data$Story))
+
+
+sentiment_polarity_score_groupby <- sentiment_polarity_score %>% 
+  group_by(element_id) %>% 
+  summarise(sentiment = sum(sentiment),sentences_no=max(sentence_id),word_count=sum(word_count),positive=count(sentiment>0))
+
+pub_data$ID <- seq.int(nrow(pub_data))
+temp <- merge(x=pub_data,y=sentiment_polarity_score_groupby,by.x="ID",by.y="element_id",all.x = TRUE)
+options(scipen = 999)
+temp$average_sentiment_score <- temp$sentiment/temp$word_count*100
+
+temp$average_sentiment_score_Sentences <- temp$sentiment/temp$sentences_no*100
+
+View(head(pub_data$Story,n=20))
+
+###############
+
+
+
+
+
+
+
+
+###
+
+temp<-pub_data$Story
+
+colnames(temp)[1] <- "word"
+
+trump_tweet_sentiment <- temp[1] %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(sentiment) 
+
+###
+
+
+load(url("https://cbail.github.io/Trump_Tweets.Rdata"))
+
+tidy_trump_tweets<- trumptweets %>%
+  select(created_at,text) %>%
+  unnest_tokens("word", text)
+
+trump_tweet_sentiment <- tidy_trump_tweets %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(created_at, sentiment) 
+
+View(head(trump_tweet_se,n=20))
---
+++ alan's code.R
@@ -0,0 +1,146 @@
+###########################################Frank
+install.packages("RColorBrewer")
+
+library(zipcode)
+library(tidyverse)
+library(usmap)
+library(ggplot2)
+library(tidytext)
+library(tm)
+library(topicmodels)
+library(wordcloud)
+library(RColorBrewer)
+
+# Reading samples
+sample <- read.csv("cleaned_data.csv", stringsAsFactors = FALSE)
+sample <- sample[1:812,]
+colnames(sample)[13] <- "zip"
+sample$zip2[sample$zip < 10000 & is.na(sample$zip) == F] <- sprintf("%05d", sample$zip[sample$zip < 10000 & is.na(sample$zip) == F])
+sample <- sample[ ,colSums(!is.na(sample)) != 0]
+sample$Story <- do.call(paste, c(sample[18:127], sep=" "))
+sample <- sample[,c(1:17,128)]
+
+# Using Tidytext to unnest the dataframe
+sampledf <- sample %>% 
+  select(Story) %>% 
+  unnest_tokens("word", Story)
+# Stopwords
+data("stop_words")
+samplesort <- sampledf %>% 
+  anti_join(stop_words) %>% 
+  count(word) %>% 
+  arrange(desc(n))
+
+# Graphing1: top 20 words in histogram
+top_20 <- samplesort[1:20,]
+top_20$word <- factor(top_20$word,
+                      levels = top_20$word[order(top_20$n, decreasing = TRUE)])
+ggplot(top_20, aes(x=word, y=n, fill=word))+
+  geom_bar(stat="identity")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  ylab("Word freq in real talks sample")+
+  xlab("")+
+  guides(fill=FALSE)
+
+# Graphing2: Corpus & Topic modeling
+# Creating Corpus
+sample_corpus <- Corpus(VectorSource(as.vector(sample$Story))) 
+sample_corpus <- tm_map(sample_corpus, removeWords, stopwords("english"))
+sample_corpus <- tm_map(sample_corpus, content_transformer(removeNumbers))
+sample_corpus <- tm_map(sample_corpus, content_transformer(removePunctuation))
+sample_corpus <- tm_map(sample_corpus, content_transformer(tolower))
+sample_corpus <- tm_map(sample_corpus, content_transformer(stemDocument), language = "english")
+# Creating dtm
+DTM <- DocumentTermMatrix(sample_corpus, control = list(wordLengths = c(2, Inf)))
+TDM <- TermDocumentMatrix(sample_corpus, control = list(wordLengths = c(2, Inf)))
+rowTotals <- apply(DTM , 1, sum)
+DTM <- DTM[rowTotals> 0, ] 
+topic_model<-LDA(DTM, k=10, control = list(seed = 321)) #### Not Working !!!!!!
+
+# Tidying the model
+topics <- tidy(topic_model, matrix = "beta")
+top_terms <- 
+  topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+# Ploting 
+top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip()
+
+# Graphing3: Wordcloud
+m <- as.matrix(TDM)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+# head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+# Graphing4: visualization of state counts for the sample
+# Building a zipprefix-state df
+data(zipcode)
+#zipprefix <- zipcode
+#zipprefix$zip <- substr(zipprefix$zip, 0, 3) # If only using prefix 
+zip <- zipcode %>% 
+  select(zip, state) %>% 
+  distinct(zip, .keep_all = TRUE)
+# Creat a new df with state and state count
+samplestate <- merge(sample, zip, by = "zip")
+samplecount <- samplestate %>% 
+  group_by(state) %>% 
+  count(state)
+# Plotting
+plot_usmap(regions = "states", data = samplecount, values = "n", lines = "red") + 
+  scale_fill_continuous(low = "white", high = "red", name = "Population (2015)", label = scales::comma) +
+  labs(title = "US States", subtitle = "This is a map.") + 
+  theme(panel.background = element_rect(colour = "black", fill = "white"), legend.position = "right")
+
+
+###########################################Alan
+#!diagnostics off
+# Using Tidytext to unnest the dataframe by two grams
+sampledf2 <- sample %>% 
+  select(Story) %>% 
+  unnest_tokens(ngram, Story, token = "ngrams",n=2)
+
+sample_separated <- sampledf2 %>%
+  separate(ngram, c("word1", "word2"), sep = " ")
+
+data("stop_words")
+sample_filtered <- sample_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+sample_counts <- sample_filtered %>% 
+  count(word1, word2, sort = TRUE)
+
+sample_united <- sample_filtered %>%
+  unite(ngram, word1, word2, sep = " ")
+
+sample_unitedcounts <- sample_united %>% 
+  count(ngram, sort = TRUE)
+
+sample_unitedcounts <- sample_unitedcounts[-1,]
+
+top_20tgrams <- sample_unitedcounts[1:20,]
+
+top_20tgrams$ngram <- factor(top_20tgrams$ngram,
+                             levels = top_20tgrams$ngram[order(top_20tgrams$n, decreasing = TRUE)])
+ggplot(top_20tgrams, aes(x=ngram, y=n, fill=ngram))+
+  geom_bar(stat="identity")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  ylab("Two grams freq in real talks sample")+
+  xlab("")+
+  guides(fill=FALSE)
+
+# Export cleaned data for LSWC
+write.csv(sample,'cleaned_data.csv')
